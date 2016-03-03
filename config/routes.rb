0d6240ad098ca3b2d07aafca25ccddeab539d706
Thang Pham <phamtoanthang@gmail.com>
2016-03-03 23:02:58 +0700

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :microposts
+  resources :users
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  root 'users#index'
 
 end
